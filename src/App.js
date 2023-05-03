f621cdd10009b446cf45c6b28706088001946fa9
Ha Chi HOANG <chibienayme@gmail.com>
2023-05-03 20:50:21 +0200

    add .vscode/setting.json for Prettier

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
-import Button from './components/Button'
+import Button from './components/Button';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Tiktok</h1>
-      <Button/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Tiktok</h1>
+            <Button />
+        </div>
+    );
 }
 
 export default App;
