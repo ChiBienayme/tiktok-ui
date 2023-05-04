c399314732e2f0ad8ef24ed1cc39fb4bc0bedc0c
Ha Chi HOANG <chibienayme@gmail.com>
2023-05-04 23:12:30 +0200

    menu more & menu item

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,25 +1,42 @@
 import { useState, useEffect } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faCircleXmark, faSpinner, faMagnifyingGlass, faEllipsisVertical, faEarthAsia, faCircleQuestion, faKeyboard } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
 
-import { Wrapper as PopperWrapper} from '~/components/Popper';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import Button from '~/components/Button';
+import Menu from '~/components/Popper/Menu';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 
 const cx = classNames.bind(styles); // to write post-item, not only camelCase
 
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia} />,
+        title: 'English'
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: 'Feedback and Help',
+        to: '/feedback'
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard shortcuts',
+    },
+];
+
 function Header() {
-    const [searchResult, setSearchResult] =  useState([])
+    const [searchResult, setSearchResult] = useState([]);
 
     useEffect(() => {
         setTimeout(() => {
-            setSearchResult([])
-        }, 0)
-    }, [])
+            setSearchResult([]);
+        }, 0);
+    }, []);
 
     return (
         <header className={cx('wrapper')}>
@@ -28,29 +45,23 @@ function Header() {
                 <img src={images.logo} alt="Tiktok" />
 
                 {/* Search */}
-                <Tippy 
+                <Tippy
                     interactive
-                    visible={searchResult.length > 0 }
+                    visible={searchResult.length > 0}
                     render={(attrs) => (
                         <div className={cx('search-result')} tabIndex="-1" {...attrs}>
-                                <PopperWrapper>
-                                    <h4 className={cx('search-title')}>
-                                        Accounts
-                                    </h4>
+                            <PopperWrapper>
+                                <h4 className={cx('search-title')}>Accounts</h4>
 
-                                    <AccountItem/>
-                                    <AccountItem/>
-                                    <AccountItem/>
-      
-                                </PopperWrapper>
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                            </PopperWrapper>
                         </div>
                     )}
                 >
                     <div className={cx('search')}>
-                        <input 
-                            placeholder="Search accounts and videos" 
-                            spellCheck={false} 
-                        />
+                        <input placeholder="Search accounts and videos" spellCheck={false} />
                         {/* Clear */}
                         <button className={cx('clear')}>
                             <FontAwesomeIcon icon={faCircleXmark} />
@@ -66,11 +77,17 @@ function Header() {
                 </Tippy>
 
                 <div className={cx('actions')}>
-                        <Button text> Upload </Button>
-                        <Button primary> Log in </Button>
-                </div>
+                    <Button text> Upload </Button>
+                    <Button primary> Log in </Button>
 
-          
+                    <Menu
+                        items={MENU_ITEMS}
+                    >
+                        <button className={cx('more-btn')}> 
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </button>
+                    </Menu>
+                </div>
             </div>
         </header>
     );
