009840d677796842e2618ad52ce521c02d6e5e9f
Ha Chi HOANG <chibienayme@gmail.com>
2023-05-04 17:42:08 +0200

    search input and search result

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,28 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/webapp-desktop/5fa49876a48136384338.png"
+                alt=""
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
